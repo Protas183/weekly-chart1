e3259fb510812eb6b8970b9c222d5b80369d245d
Artem <protastema1803@gmail.com>
2023-10-12 16:17:01 +0300

    Fix indent menu in selectLocation

--- src/components/SelectLocation/SelectLocation.jsx
+++ src/components/SelectLocation/SelectLocation.jsx
@@ -11,18 +11,23 @@ const customStyles = {
         borderColor: state.isFocused ? '#6e7c7c' : '#ccc',
         boxShadow: state.isFocused ? '0 0 0 1px #6e7c7c' : null,
         '&:hover': {
-        borderColor: '#6e7c7c', 
+        borderColor: '#6e7c7c',
         },
     }),
     option: (provided, state) => ({
-        ...provided,
+      ...provided,
+
         backgroundColor: state.isSelected ? '#6e7c7c' : '#f4f4f4',
         color: state.isSelected ? '#fff' : '#333',
         '&:hover': {
         backgroundColor: '#6e7c7c',
         color: '#fff',
         },
-    }),
+  }),
+    menu: (provided) => ({
+    ...provided,
+    top: "35px", // убираем отступ
+  }),
 };
 
 
